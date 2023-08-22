f723c2938b5a7f4e9ce66206601dc048400fc8aa
jianhan <jianhan.thu@gmail.com>
2023-08-22 22:59:53 +0800

    add period param

--- gen_compare_video.py
+++ gen_compare_video.py
@@ -15,8 +15,6 @@ def combine_dual_imgs(img1, img2, delimer_ind, delimer_width=3, delimer_color=[0
         right_ind = left_ind + delimer_width
     if right_ind == (w-1):
         left_ind = right_ind - delimer_width
-    print(left_ind, right_ind)
-    # import ipdb; ipdb.set_trace()
     new_img = copy.deepcopy(img1)
     new_img[:,left_ind:right_ind] = delimer_color
     new_img[:,right_ind:] = img2[:,right_ind:]
@@ -28,14 +26,16 @@ if __name__ == '__main__':
     save_dir = '/home/hanj/projects/pointcloud_vis_tools/compare_frames'
     os.makedirs(save_dir, exist_ok=True)
     ptr = None
-    step = 100
-    signed_step = -step
+    fps = 5
+    period = 2 # time (s) for washing the whole screen
     for i, img_name in enumerate(sorted(os.listdir(imgs_dir1))):
         img1 = cv2.imread(osp.join(imgs_dir1, img_name))
         img2 = cv2.imread(osp.join(imgs_dir2, img_name))
         h, w, c = img1.shape
         if ptr is None:
             ptr = w
+            step = w / fps / period
+            signed_step = -step
         ptr += signed_step
         if ptr < 0:
             ptr = 0
@@ -43,8 +43,7 @@ if __name__ == '__main__':
         elif ptr >= w:
             ptr = w - 1
             signed_step = -step
-        # print(ptr)
-        compare_img = combine_dual_imgs(img1, img2, delimer_ind=ptr, delimer_width=6)
+        compare_img = combine_dual_imgs(img1, img2, delimer_ind=ptr, delimer_width=4)
         save_file = osp.join(save_dir, img_name)
         cv2.imwrite(save_file, compare_img)
-    os.system(f'ffmpeg -framerate 5 -i {save_dir}/%4d.png demo.mp4')
+    os.system(f'ffmpeg -framerate {fps} -i {save_dir}/%4d.png demo.mp4')
